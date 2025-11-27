b7e4bb4e1f3c47c7dfe2c1c12ae1bbf38a6d844e
joakmannn <165683883+joakmannn@users.noreply.github.com>
2025-11-27 16:23:58 +0100

    Initialise express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello World");
+import express, { Request, Response } from 'express';
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(5001, () => {
+  console.log("Server running on port 5001");
+}); 
