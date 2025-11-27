6689715c1ef3e5b0a83a441d65bc0ed2cc37eefc
joakmannn <165683883+joakmannn@users.noreply.github.com>
2025-11-27 16:49:05 +0100

    import subscriptionRoutes in index

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response } from 'express';
 import cors from "cors";
+import subscriptionRoutes from './routes/subscription.routes';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/subscriptions', subscriptionRoutes);
 
 app.listen(5001, () => {
   console.log("Server running on port 5001");
